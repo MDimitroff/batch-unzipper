f73ae676c4fa1ebb37f43d6d93b5a20aae176fd5
Martin Dimitrov <MDI@milestone.dk>
2021-03-13 11:05:50 +0200

    Initial commit

---
+++ BatchUnzipper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BatchUnzipper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
