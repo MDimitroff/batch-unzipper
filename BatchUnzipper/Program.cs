9e7f5ed6b8dc2c5f86293ed4a966096907a4577c
Martin Dimitrov <mdi@milestone.dk>
2021-03-13 11:55:00 +0200

    Initial commit

--- BatchUnzipper/Program.cs
+++ BatchUnzipper/Program.cs
@@ -6,7 +6,13 @@ namespace BatchUnzipper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Insert the full path to the directory containing the .zip files: ");
+            string zipDir = Console.ReadLine().Trim();
+            Console.Write("Insert the full path to the directory where you want to unzip your files: ");
+            string unzippedDir = Console.ReadLine().Trim();
+
+            Console.WriteLine($"zipDir: {zipDir}; unzippedDir: {unzippedDir}");
+            Console.ReadLine();
         }
     }
 }
